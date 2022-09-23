8109634b261fab38b8b2e146a296b7407b450380
Youssef Galal <youssefgalalnazem@gmail.com>
2022-09-23 21:21:37 +0200

    Change Components to Statless Functional Components

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,54 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Product extends Component {
-
-  render() {
-    return (
-      <div className="row">
-        <div className="col">
-          <span className="m-2">{this.props.children}</span>
-          <span>{this.props.product.name}</span>
-        </div>
-        <div className="col text-end">
-          <span
-            className={`badge bg-${
-              this.props.product.quantity === 0 ? "warning text-dark" : "primary text-light"
-            } m-2`}
-          >
-            {this.props.product.quantity}
-          </span>
-          <button
-            onClick={() => {
-              this.props.onIncrement(this.props.product.id);
-            }}
-            className="btn btn-outline-primary btn-sm fas fa-add m-1"
-          >
-          </button>
-          <button
-            onClick={() => {
-              this.props.onDecrement(this.props.product.id);
-            }}
-            className="btn btn-outline-primary btn-sm fas fa-minus m-1"
-          >
-          </button>
-          <button
-            onClick={() => {
-              this.props.onReset(this.props.product.id);
-            }}
-            className="btn btn-outline-warning btn-sm fas fa-remove m-1"
-          >
-          </button>
-          <button className="btn btn-light btn-sm m-1"
-            onClick={() => {
-              this.props.onDelete(this.props.product.id);
-            }}
-          >
-            <i className="fas fa-trash text-danger"></i>
-          </button>
-        </div>
+const Product = (props) => {
+  return (
+    <div className="row">
+      <div className="col">
+        <span className="m-2">{props.children}</span>
+        <span>{props.product.name}</span>
       </div>
-    );
-  }
-}
+      <div className="col text-end">
+        <span
+          className={`badge bg-${
+            props.product.quantity === 0
+              ? "warning text-dark"
+              : "primary text-light"
+          } m-2`}
+        >
+          {props.product.quantity}
+        </span>
+        <button
+          onClick={() => {
+            props.onIncrement(props.product.id);
+          }}
+          className="btn btn-outline-primary btn-sm fas fa-add m-1"
+        ></button>
+        <button
+          onClick={() => {
+            props.onDecrement(props.product.id);
+          }}
+          className="btn btn-outline-primary btn-sm fas fa-minus m-1"
+        ></button>
+        <button
+          onClick={() => {
+            props.onReset(props.product.id);
+          }}
+          className="btn btn-outline-warning btn-sm fas fa-remove m-1"
+        ></button>
+        <button
+          className="btn btn-light btn-sm m-1"
+          onClick={() => {
+            props.onDelete(props.product.id);
+          }}
+        >
+          <i className="fas fa-trash text-danger"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Product;
--- src/components/shoppingCart.jsx
+++ src/components/shoppingCart.jsx
@@ -1,38 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import Product from "./product";
 
-class ShoppingCart extends Component {
-  render() {
-    return (
-      <>
-        <h1 className="m-2">Shopping Cart</h1>
-        {this.props.products.map((product) => (
-          <Product
-            key={product.id}
-            product={product}
-            onDelete={this.props.onProductDeleteHandler}
-            onIncrement={this.props.onProductIncrementHandler}
-            onDecrement={this.props.onProductDecrementHandler}
-            onReset={this.props.onProductResetHandler}
-          >
-            <img width="32px" src={product.image} alt={`${product.name}`} />
-          </Product>
-        ))}
-        <button
-          className="btn btn-warning text-dark m-2"
-          onClick={this.props.onAllProductsResetHandler}
+const ShoppingCart = (props) => {
+  return (
+    <>
+      <h1 className="m-2">Shopping Cart</h1>
+      {props.products.map((product) => (
+        <Product
+          key={product.id}
+          product={product}
+          onDelete={props.onProductDeleteHandler}
+          onIncrement={props.onProductIncrementHandler}
+          onDecrement={props.onProductDecrementHandler}
+          onReset={props.onProductResetHandler}
         >
-          Reset all
-        </button>
-        <button
-          className="btn btn-danger text-light m-2"
-          onClick={this.props.onAllProductsDeleteHandler}
-        >
-          Delete all
-        </button>
-      </>
-    );
-  }
-}
+          <img width="32px" src={product.image} alt={`${product.name}`} />
+        </Product>
+      ))}
+      <button
+        className="btn btn-warning text-dark m-2"
+        onClick={props.onAllProductsResetHandler}
+      >
+        Reset all
+      </button>
+      <button
+        className="btn btn-danger text-light m-2"
+        onClick={props.onAllProductsDeleteHandler}
+      >
+        Delete all
+      </button>
+    </>
+  );
+};
 
 export default ShoppingCart;
