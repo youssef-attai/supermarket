2e67296d42b6c68185a16d1b723562fd445c8a86
Youssef Galal <youssefgalalnazem@gmail.com>
2022-09-23 21:33:35 +0200

    Fix badge-pill

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,15 @@ import React from "react";
 const NavBar = (props) => {
   return (
     <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand ms-3" href="/">
+      <span className="navbar-brand ms-3">
         E-Commerce App
+      </span>
+      <a className="btn btn-dark position-relative me-3" href="/">
+        <i className="fas fa-shopping-cart"></i>
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          {props.count}
+        </span>
       </a>
-      <span className="badge bg-danger me-2">{props.count}</span>
     </nav>
   );
 };
