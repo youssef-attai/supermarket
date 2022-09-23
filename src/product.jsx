7e19103757c7afd19aa2b6cca24fbb3107a4cfbf
Youssef Galal <youssefgalalnazem@gmail.com>
2022-09-23 18:20:38 +0200

    ch03-vid03

--- src/product.jsx
+++ src/product.jsx
@@ -4,11 +4,18 @@ class Product extends Component {
   state = {
     name: "Burger",
     count: 3,
+    imageUrl: "https://www.svgrepo.com/show/32704/burger.svg",
   };
-  
+
   render() {
     return (
       <div>
+        <img
+          className="m-2"
+          src={this.state.imageUrl}
+          width="32px"
+          alt="Product image"
+        />
         <span>{this.state.name}</span>
         <span className="badge bg-primary m-2">{this.state.count}</span>
       </div>
