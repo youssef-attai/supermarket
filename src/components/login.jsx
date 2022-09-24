56867bc615dd69d037a9020ea625fb98dcc18f38
Youssef Galal <youssefgalalnazem@gmail.com>
2022-09-25 00:29:46 +0200

    Add form validation using Joi

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 
 class Login extends Component {
   state = {
     username: "",
     password: "",
+    errors: [],
   };
+
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  };
+
+  validate() {
+    const errors = [];
+    const state = { ...this.state };
+    delete state.errors;
+    const { error } = Joi.validate(state, this.schema, { abortEarly: false });
+    if (error) {
+      error.details.forEach((err) => {
+        errors.push(err.message);
+      });
+    }
+    return errors;
+  }
 
   onSubmitHandler = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+
+    if (errors.length > 0) {
+      this.setState({ errors: errors });
+      return;
+    }
+
+    console.log("Submit");
+    this.setState({
+      username: "",
+      password: "",
+      errors: [],
+    });
   };
 
   onChangeHandler = (e) => {
@@ -20,7 +54,10 @@ class Login extends Component {
     return (
       <>
         <h1 className="m-4 text-center">Login</h1>
-        <form onSubmit={this.onSubmitHandler}>
+        {this.state.errors.map((err) => (
+          <div className="alert alert-danger">{err}</div>
+        ))}
+        <form onSubmit={this.onSubmitHandler} className="d-flex flex-column">
           <div className="mb-3">
             <input
               type="text"
@@ -47,7 +84,7 @@ class Login extends Component {
               name="password"
             />
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary mb-3">
             Submit
           </button>
         </form>
